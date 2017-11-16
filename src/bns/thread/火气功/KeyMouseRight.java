71aafec912ccb2bc943b763309271858d5b998f8
ginge <ginge@sina.com>
2017-11-16 16:56:00 +0800

    火气功2

---
+++ src/bns/thread/火气功/KeyMouseRight.java
@@ -0,0 +1,38 @@
+package bns.thread.火气功;
+
+import bns.BnsConst;
+import bns.BnsKeyAgent;
+import bns.thread.KeyThreadAbstract;
+
+public class KeyMouseRight extends KeyThreadAbstract {
+	private org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(KeyMouseRight.class);
+	
+	public KeyMouseRight() {
+		super(BnsKeyAgent.getInstance().getAgentKeyText("MOUSE_LISTENER_RIGHT"));
+		this.isContinueFight = true;
+		this.isSubThread = true;
+	}
+	
+	/**
+	 * 执行自定义技能
+	 * @return true:循环执行   false:单次执行
+	 */
+	protected boolean runSkill() throws InterruptedException, Exception {
+		log.debug("KeyMouseRight");
+		
+		int cnt = 0;
+		
+		//蒲公英
+		while (cnt <= 3) {
+			mousePress(BnsConst.MOUSE_RIGHT);
+			if (isColorOK("蒲公英")) {
+				keyPress("4");
+			}
+			
+			doSleep(100);
+			cnt++;
+		}
+		
+		return false;
+	}
+}
